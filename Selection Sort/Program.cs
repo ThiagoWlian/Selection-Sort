1513fe9a4fc56f0892fc141ad32dab7071acd318
Thiago Cordeiro Wlian <thiagowlian@outlook.com.br>
2022-07-02 21:39:12 -0300

    Algoritomo de Ordenacao

---
+++ Selection Sort/Program.cs
@@ -0,0 +1,35 @@
+﻿int[] vetorDesordenado = {7,9,4,40,6};
+
+
+int encontraIndiceMenorNumero(int inicioIndice)
+{
+    int indiceMenorNumero = inicioIndice;
+    for (int indice = inicioIndice; indice < vetorDesordenado.Length; indice++)
+    {
+        if(vetorDesordenado[indice] < vetorDesordenado[indiceMenorNumero])
+        {
+            indiceMenorNumero = indice;
+        }
+    }
+    return indiceMenorNumero;
+}
+
+void ordenaVetorNumerico()
+{
+    for(int indice = 0; indice < vetorDesordenado.Length; indice++)
+    {
+        int indiceMenorNumero = encontraIndiceMenorNumero(indice);
+        int atualValor = vetorDesordenado[indice];
+        int maiorValor = vetorDesordenado[indiceMenorNumero];
+
+        vetorDesordenado[indice] = maiorValor;
+        vetorDesordenado[indiceMenorNumero] = atualValor;
+    }
+}
+
+ordenaVetorNumerico();
+
+foreach (int indice in vetorDesordenado)
+{
+    Console.WriteLine(indice);
+}
